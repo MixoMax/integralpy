0350b11d0c8464583ec5f4b56cd5525c14502071
Linus Horn <70818329+MixoMax@users.noreply.github.com>
2022-11-21 11:04:21 +0100

    fast fertig mit intervallen
    gibt Error, das die indexe angeblich keine int() sind.

--- integral.py
+++ integral.py
@@ -2,10 +2,32 @@ import math
 import string
 import matplotlib.pyplot as plt
 
+#To Do
+#Funktionen nur in intervallen hinzufügen: Syntax: "x**2,i0-20;x**3,i20-25"
+#
+
 function = str(input("Funktion: "))
 stepsize = int(input("Schritgröße (Schritte pro Einheit): "))
 minx = float(eval(input("von: ")))
 maxx = float(eval(input("bis: ")))
+
+def splitFunction(function):
+
+    functionArray = []
+    if(function.count("i") == 0):
+        return function
+    else:
+        for l in range(function.count(";")):
+            index = function.find("i")
+            
+            funcxy = [function[int(0):int(index-1)], function[int(index+1):int(function.find("-")), int(function.find("-")+1):int(function.find(";")-1)]]
+            if len(function) > index:
+                function = function[int(0) : int(index) : ] + function[int(index + 1) : :]
+                function = function[int(function.find("-")) : ] + function[int(function.find("-")+1) : :]
+                function = function[int(0) : int(function.find(";")) : ] + function[int(function.find(";")+1) : :]
+            functionArray.append(funcxy)
+        
+        return functionArray
 
 def getY(xcoord, function):
     x = str(xcoord)
@@ -28,20 +50,38 @@ def plot(xArray, yArray):
         if(i+1 < len(xArray)):
             ax.plot([xArray[i+1],xArray[i]], [yArray[i], yArray[i]], color="r")
     plt.show()
+    
+def calcXY(stepsize, functionArray):
+    yArray = []
+    xArray = []
+    if(type(functionArray is int)):
+        print(functionArray)
+        for xcoord in range(len(functionArray)*stepsize+1):
+            xcoord = (xcoord/stepsize)
+            yArray.append(getY(xcoord, function = functionArray))
+            xArray.append(xcoord)
+        return xArray, yArray
+    
+    else:        
+        for i in len(functionArray):
+            j = abs(functionArray[i][1]-functionArray[i][2])
+            for xcoord in range(j*stepsize):
+                xcoord = (xcoord/stepsize) + functionArray[i][1]
+                yArray.append(getY(xcoord, function=functionArray[i][0]))
+                xArray.append(xcoord)
+        return xArray, yArray
+            
 
-yArray = []
-xArray = []
-for xcoord in range(int(stepsize*(abs(minx-maxx))+1)):
-    xcoord = xcoord + (minx*stepsize)
-    xcoord = float(xcoord / stepsize)
-    yArray.append(getY(xcoord, function))
-    xArray.append(xcoord)
+def calcArea(xArray, yArray):
+    QuadTotal = []
+    TriangleTotal = []
+    for i in range(len(xArray)-1):
+        QuadTotal.append(calcSize(xArray[i], 0, xArray[i+1], yArray[i+1]))
+        TriangleTotal.append((calcSize(xArray[i], yArray[i], xArray[i+1], yArray[i+1]))/2)
+    return sum(TriangleTotal) + sum(QuadTotal)
 
-QuadTotal = []
-TriangleTotal = []
-for i in range(len(xArray)-1):
-    QuadTotal.append(calcSize(xArray[i], 0, xArray[i+1], yArray[i+1]))
-    TriangleTotal.append((calcSize(xArray[i], yArray[i], xArray[i+1], yArray[i+1]))/2)
-
-print(sum(TriangleTotal)+ sum(QuadTotal))
+functionArray = splitFunction(function)
+xArray, yArray = calcXY(stepsize, functionArray)
+print(calcArea(xArray, yArray))
 plot(xArray, yArray)
+#
